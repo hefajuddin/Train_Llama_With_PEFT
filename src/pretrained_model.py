cec0ea8252a23f21e8442a6ea2138ba890957191
Hefaj Uddin <hefajuddin101@gmail.com>
2024-12-14 01:32:14 +0600

    update pretrained model

--- src/pretrained_model.py
+++ src/pretrained_model.py
@@ -2,4 +2,4 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 model_name = "meta-llama/Llama-2-7b-hf"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-pretrained_model = AutoModelForCausalLM.from_pretrained(model_name)
+pretrained_model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_4bit=True)
