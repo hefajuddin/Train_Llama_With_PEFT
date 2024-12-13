b78c6b4833322ef954bb6ed220380a70531c48d7
Hefaj Uddin <hefajuddin101@gmail.com>
2024-12-14 01:13:46 +0600

    add pretrained model Llama-2-7b-hf

---
+++ src/pretrained_model.py
@@ -0,0 +1,5 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+model_name = "meta-llama/Llama-2-7b-hf"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+pretrained_model = AutoModelForCausalLM.from_pretrained(model_name)
